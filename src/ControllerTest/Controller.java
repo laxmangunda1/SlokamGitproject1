631fc1319ecdd2273e17ffcae6a45628504701b9
Your Name <laxmangunda009@gmail.comgit config --global user.email laxmangunda009@gmail.com>
2023-07-27 19:45:58 +0530

    gitproject

---
+++ src/ControllerTest/Controller.java
@@ -0,0 +1,13 @@
+package ControllerTest;
+
+public class Controller {
+public  void Test() {
+	System.out.println("Test");
+	
+}
+public static void main(String[] args) {
+	Controller Controller=new Controller();
+	Controller.Test();
+	
+}
+}
